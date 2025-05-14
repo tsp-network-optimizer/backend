e8132afd56f9132d6d33ef35e17abdc095ee4a29
Julian Ramos <julianc.ramos21@gmail.com>
2025-05-14 18:22:08 -0500

    test graph flow from main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
 
+from app.services.graph_loader import get_graph
+from app.services.process_nodes import load_points_to_visit, process_points_into_graph
+
 app = FastAPI()
 
 @app.get("/prueba")
 def prueba():
     return {"message": "prueba"}
+
+
+
+
+def main():
+    # simular grafo
+    G = get_graph()
+    print(f"Grafo inicial: {G.number_of_nodes()} nodos, {G.number_of_edges()} aristas")
+
+    # cargar los puntos desde .txt
+    file_path = "data/points.txt" 
+    points = load_points_to_visit(file_path)
+    print(f"Leídos {len(points)} puntos desde archivo.")
+
+    # procesar nuevos nodos leídos
+    final_node_ids = process_points_into_graph(G, points)
+    print("Nodos a visitar (IDs en el grafo):", final_node_ids)
+
+    #nuevo estado del grafo, con las modificaciones
+    print(f"Grafo actualizado: {G.number_of_nodes()} nodos, {G.number_of_edges()} aristas")
+
+if __name__ == "__main__":
+    main()
