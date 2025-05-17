dfd94443037f11d1c4271fa2ccbf411b9f6e9491
Julian Ramos <julianc.ramos21@gmail.com>
2025-05-17 16:15:51 -0500

    feat: load real graph from OSM file instead of simulation

--- app/services/graph_loader.py
+++ app/services/graph_loader.py
@@ -2,6 +2,9 @@ import networkx as nx #para grafos
 import random
 from geopy.distance import geodesic #para distancias
 
+
+import osmnx as ox #para cargar archivo OSM en el grafo
+
 # variable para el grafo, al inicio es nulo
 _loaded_graph: nx.Graph | None = None
 
@@ -11,46 +14,39 @@ def get_graph() -> nx.Graph:
 
 
 
-#simula la creación del grafo, dado que aún no hay archivo específico para realizar lectura
+#carga datos al grafo usando archivo OSM en data/chapinero.osm
 def load_graph() -> nx.Graph:
     global _loaded_graph
 
+    #si ya está cargado lo retorna
     if _loaded_graph is not None:
-        # si ya está cargado lo retorna
-        return _loaded_graph  
+        return _loaded_graph
 
-    G = nx.Graph()
+    filepath = "data/chapinero.osm" 
 
-    # coordenadas de Bogotá para simular
-    base_lat = 4.65
-    base_lng = -74.1
+    # carga el grafo según la ruta
+    G = ox.graph_from_xml(filepath, simplify=True)
 
-    # Crea 20 nodos aleatorios
-    for i in range(20):
-        lat = base_lat + random.uniform(-0.03, 0.03)
-        lng = base_lng + random.uniform(-0.03, 0.03)
-        G.add_node(i, latitude=lat, longitude=lng)
 
-    # Conectar cada nodo con sus 3 más cercanos (por distancia geodésica)
-    nodes = list(G.nodes(data=True))
-    for i, (id_a, data_a) in enumerate(nodes):
-        coord_a = (data_a["latitude"], data_a["longitude"])
-        distances = []
+    # se convierte a grafo no dirigido
+    if G.is_directed():
+        G = G.to_undirected()
 
-        for j, (id_b, data_b) in enumerate(nodes):
-            if id_a == id_b:
-                continue
-            coord_b = (data_b["latitude"], data_b["longitude"])
-            dist = geodesic(coord_a, coord_b).meters
-            distances.append((id_b, dist))
+    # Asegurar lat/lon en cada nodo
+    for node_id, data in G.nodes(data=True):
+        data["latitude"] = data.get("y")
+        data["longitude"] = data.get("x")
 
-        distances.sort(key=lambda x: x[1])
-        closest = distances[:3]
+    # Calcular peso de la arista entre nodos (se usa distancia geodésica) 
+    for u, v, data in G.edges(data=True):
+        node_1 = G.nodes[u]
+        node_2 = G.nodes[v]
 
-        for target_id, dist in closest:
-            if not G.has_edge(id_a, target_id):
-                G.add_edge(id_a, target_id, weight=dist)
+        coord_1 = (node_1["latitude"], node_1["longitude"])
+        coord_2 = (node_2["latitude"], node_2["longitude"])
+
+        dist = geodesic(coord_1, coord_2).meters
+        data["weight"] = dist
 
     _loaded_graph = G
     return G
-
